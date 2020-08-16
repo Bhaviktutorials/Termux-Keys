245eab21da5b9a786ea0d4f020197d72138d1e61
Bhaviktutorials <64035221+Bhaviktutorials@users.noreply.github.com>
2020-08-16 12:03:46 +0530

    new 1

---
+++ termux-keys.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Installing the requirements files
+pkg  update -y 
+pkg  upgrade -y
+pkg install python -y  
+apt-get install ruby -y
+pip install lolcat
+gem install lolcat
+apt-get install ncurses-utils -yq --silent
+clear
+echo " "
+echo " "
+echo "           < ━━━━━━━━━━━━ [★] CREATED BY ASHISH [★] ━━━━━━━━━━━━ > " |lolcat
+echo " "
+echo "  
+                 ▀█▀ █▀▀ █▀█ █▀▄▀█ █ █ ▀▄▀ ▄▄ █▄▀ █▀▀ █▄█ █▀
+                  █  ██▄ █▀▄ █ ▀ █ █▄█ █ █    █ █ ██▄  █  ▄█  " |lolcat		  
+
+echo " "
+echo " "
+echo "           < ━━━━━━━━━━━━━ [★] BHAVIK TUTORIALS [★] ━━━━━━━━━━━━ > " |lolcat
+
+   #Installing Termux-keys
+./key
